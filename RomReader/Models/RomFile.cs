213a7c993d9f956eecaef522399c407dcacae56f
glt0807 <glt0807@mavs.uta.edu>
2023-01-29 21:43:50 -0600

    Created better view Began adding box art API Wrapper

--- RomReader/Models/RomFile.cs
+++ RomReader/Models/RomFile.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Globalization;
+
 namespace RomReader.Models;
 
 public enum _ConsoleType
@@ -11,14 +13,28 @@ public enum _ConsoleType
 
 public abstract class RomFile
 {
+    
+    public _ConsoleType ConsoleType { get; set; }
+    public string Title { get; set; }
+    
     protected RomFile(_ConsoleType consoleType, string title)
     {
         ConsoleType = consoleType;
         Title = title;
     }
+
+    public string GetConsoleTypeString()
+    {
+        return ConsoleType.ToString();
+    }
+
+    public string GetTitleCaseTitle()
+    {
+        TextInfo ti = new CultureInfo("en-US", false).TextInfo;
+        return ti.ToTitleCase(ti.ToLower(Title));
+    }
     
-    public _ConsoleType ConsoleType { get; set; }
-    public string Title { get; set; }
+    
     
     
 }
---
+++ RomReader/Util/IgdbUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using IGDB;
+using IGDB.Models;
+
+namespace RomReader.Util;
+
+public class IgdbUtils
+{
+    public IGDBClient igdb;
+    
+    public IgdbUtils()
+    {
+        igdb = new IGDBClient(
+            Environment.GetEnvironmentVariable("IGDB_CLIENT_ID"),
+            Environment.GetEnvironmentVariable("IGDB_CLIENT_SECRET")
+        );
+    }
+
+    public async Task<string> GetIDByTitle(string title)
+    {
+        var games = await igdb.QueryAsync<Game>(IGDBClient.Endpoints.Games, query: $"fields name,cover; where name {title};");
+        var artworkImageId = games.First().Artworks.Values.First().ImageId;
+
+        return IGDB.ImageHelper.GetImageUrl(imageId: artworkImageId, size: ImageSize.CoverBig, retina: false);
+    }
+}
